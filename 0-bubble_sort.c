13507394d8fb1c321be7429395b3602406c3e1b7
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-06-12 01:34:40 +0000

    tree

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - the Bubble sort algorithm
+ * @array: The arry to be sorted
+ *@size: the size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, tmp;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (i = 0 ; i < size - 1; i++)
+	{
+		for (j = 0 ; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
