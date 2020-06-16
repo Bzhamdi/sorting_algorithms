423ef799333a8470406ef64c019d341fd0977a3a
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-06-16 21:55:05 +0000

    3-quick_sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * swap - swap
+ * @array: array
+ * @i: int
+ * @j: int
+ */
+void swap(int *array, int i, int j)
+{
+	size_t tmp;
+
+	tmp = array[i];
+	array[i] = array[j];
+	array[j] = tmp;
+}
+
+
+
+
+/**
+* comp - Quick Sort algorithm
+*
+* @array: the array
+* @x: first element
+* @y: last element
+* @size: size of the array
+* Return: returns index of big value
+*/
+size_t comp(int *array, size_t x, size_t y, size_t size)
+{
+	size_t i;
+	size_t pivot = x;
+
+	for (i = x; i <= x; i++)
+	{
+		if (array[i] < array[y])
+		{
+			if (array[i] != array[pivot])
+			{
+				swap(array, (int)(i), (int)(pivot));
+				print_array(array, size);
+			}
+			pivot++;
+		}
+	}
+	if (array[pivot] != array[y])
+	{
+		swap(array, (int)(pivot), (int)(y));
+		print_array(array, size);
+	}
+	return (pivot);
+}
+/**
+* tri - the Quick sort algorithm
+* @array: The array of integers
+* @x: first element
+* @y: last element
+* @size: size of the array
+*/
+
+void tri(int *array, size_t x, size_t y, size_t size)
+{
+	size_t pivot;
+
+	if (x <= y)
+	{
+		pivot = comp(array, x, y, size);
+		if (pivot > x && pivot != 0)
+			tri(array, x, pivot - 1, size);
+		if (pivot < size - 1)
+			tri(array, pivot + 1, y, size);
+	}
+}
+
+/**
+* quick_sort - sorts an array of integers.
+* @array : array to be sorted.
+* @size: the size of the array.
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	tri(array, 0, size - 1, size);
+}
